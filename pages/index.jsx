a52424d52965088b08461b5ce99788f13f9a3a8e
Joshua Chen <josheewa7@gmail.com>
2024-01-13 17:54:13 -0500

    Shrunk gallery

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import ProductGallery from '../components/ProductGallery'
 
 const Home = () => {
@@ -6,7 +5,7 @@ const Home = () => {
   return (
     <>
       <main className="mx-auto max-w-[1960px] p-4">
-      <div className="bg-black">
+      <div className="bg-black w-1/2">
           <ProductGallery startingImagePublicId="photo-repo/DSC_1406_malfbo" tag="city" />
         </div>
       </main>
